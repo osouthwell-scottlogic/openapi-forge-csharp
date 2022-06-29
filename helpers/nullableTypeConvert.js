1e9d2a1c7ef4f0f064343507e282a3d2a656e430
murcikan@scottlogic.com <murcikan@scottlogic.com>
2022-06-29 14:44:31 +0100

    chore: improved handling of params with defaults

--- helpers/nullableTypeConvert.js
+++ helpers/nullableTypeConvert.js
@@ -1,9 +1,10 @@
 const Handlebars = require("handlebars");
-const typeConvert = require('./typeConvert');
+const typeConvert = require("./typeConvert");
+const hasDefault = require("./hasDefault");
 
 const nullableTypeConvert = (schema, optional) => {
     const schemaType = typeConvert(schema);
-    if (!optional) {
+    if (!optional || hasDefault(schema.default)) {
         return new Handlebars.SafeString(schemaType);
     }
     return ['bool', 'DateTime', 'float', 'double', 'int', 'long'].some(s => s === schemaType.toLowerCase()) ? `${schemaType}?` : schemaType;
