b6427adea190618bf574b7a38e74c2506416eadd
murcikan@scottlogic.com <murcikan@scottlogic.com>
2022-06-29 11:49:47 +0100

    chore: fixed isStringParam condition

--- helpers/createQueryString.js
+++ helpers/createQueryString.js
@@ -1,5 +1,7 @@
 const Handlebars = require("handlebars");
 
+const newLineAndAlignment = "\n\t\t\t\t\t";
+
 const getQueryParameters = (params) => {
     return Array.isArray(params)
         ? params.filter(p => p.in === 'query' && p.name !== 'body')
@@ -8,11 +10,11 @@ const getQueryParameters = (params) => {
 
 const isArrayParam = (param) => param.schema.type === 'array';
 
-const isStringArrayParam = (param) => param.schema.type === 'array' && param.schema.items && param.schema.items.type === 'string';
+const isStringArrayParam = (param) => param.schema.type === 'array' && param.schema.items && param.schema.items.type === 'string' && (param.schema.items.format === undefined || param.schema.items.format === 'string');
 
-const isStringParam = (param) => param.schema.type === 'string';
+const isStringParam = (param) => param.schema.type === 'string' && (param.schema.format === undefined || param.schema.format === 'string');
 
-const assQueryParam = (param) => isStringParam(param) ? `WebUtility.UrlEncode(${param.name})` : param.name;
+const asQueryParam = (param) => isStringParam(param) ? `WebUtility.UrlEncode(${param.name})` : param.name;
 
 const createQueryString = (params) => {
 
@@ -22,14 +24,14 @@ const createQueryString = (params) => {
         return "";
     }
 
-    let queryStringCode = `var queryString = new StringBuilder();` + "\n\t\t\t\t\t";
+    let queryStringCode = `var queryString = new StringBuilder();` + newLineAndAlignment;
 
     for (let i = 0; i < queryParams.length; i++) {
         const queryString = isArrayParam(queryParams[i])
             ? `{string.Join("&", ${queryParams[i].name}.Select(p => $"${queryParams[i].name}={${isStringArrayParam(queryParams[i]) ? "WebUtility.UrlEncode(p)" : "p"}}"))}`
-            : `${queryParams[i].name}={${assQueryParam(queryParams[i])}}`;
+            : `${queryParams[i].name}={${asQueryParam(queryParams[i])}}`;
 
-        queryStringCode += (`queryString.Append($"${i === 0 ? '?' : '&'}${queryString}` + "\");\n\t\t\t\t\t");
+        queryStringCode += (`queryString.Append($"${i === 0 ? '?' : '&'}${queryString}` + "\");" + newLineAndAlignment);
     }
 
     return new Handlebars.SafeString(queryStringCode);
