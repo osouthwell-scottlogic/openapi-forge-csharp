8cd8a1d243362e146f325897e92df7fe15cfdce9
murcikan@scottlogic.com <murcikan@scottlogic.com>
2022-07-18 10:12:19 +0100

    chore: added executable example

---
+++ example/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using OpenApiForge;
+
+var config = new Configuration
+{
+    BasePath = "/api/v3",
+    Servers = new[] { "https://petstore3.swagger.io" }
+};
+
+var services = new ServiceCollection();
+
+services.AddSingleton(config);
+services.RegisterApiClient(config);
+
+using var serviceProvider = services.BuildServiceProvider();
+
+var api = serviceProvider.GetRequiredService<IApiClient>();
+
+Console.WriteLine("Getting inventory..");
+
+var inventory = await api.getInventory();
+
+Console.WriteLine("Inventory:");
+Console.WriteLine(JsonSerializer.Serialize(inventory));
